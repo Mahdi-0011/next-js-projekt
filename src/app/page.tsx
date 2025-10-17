f9ff38914b6a73045ea2f863af671bdd5fd481a4
Mahdi <mahdi.mousavi0011@gmail.com>
2025-10-17 09:47:43 +0200

    Delete unnesesery files.

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import RootLayout from "./layout"
+
+
+export default function Home() {
+  return (
+    <div>
+      <RootLayout children={undefined} />
+      <h1>Hello Mahdi</h1>
+    </div>
+  );
+}
