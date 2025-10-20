3780454f8c3ac9bd8bb513d57ed182db8bf9a40e
Mahdi <mahdi.mousavi0011@gmail.com>
2025-10-20 15:10:25 +0200

    add bilAgare route.

---
+++ src/app/api/bilAgare/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import db from '../../db/db';
+import type { BilAgare } from '../../types/bilAgare';
+import type { RowDataPacket } from 'mysql2';
+
+export async function GET() {
+  try {
+    // Hämta alla rader från tabellen bil ägare
+    const [rows] = await db.query<RowDataPacket[]>("SELECT * FROM `bil ägare`");
+    return NextResponse.json(rows as BilAgare[]);
+    console.log(rows);
+  } catch (err :any) {
+    console.error('Databasfel:', err);
+    return NextResponse.json({ message: 'Databasfel', error: err.message }, { status: 500 });
+  }
+}
