00376c14287e5c40429206af81a29816893cf547
Mahdi <mahdi.mousavi0011@gmail.com>
2025-10-21 10:26:59 +0200

    Add GET from bilproblem.

---
+++ src/app/api/bilproblem/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+import db from '../../db/db';
+import type { problem } from '../../types/bilproblem';
+import type { RowDataPacket } from 'mysql2';
+
+export async function GET() {
+  try {
+    // Hämta alla rader från tabellen bil ägare
+    const [rows] = await db.query<RowDataPacket[]>("SELECT * FROM `bilproblem`");
+    return NextResponse.json(rows as problem[]);
+  } catch (err :any) {
+    console.error('Databasfel:', err);
+    return NextResponse.json({ message: 'Databasfel', error: err.message }, { status: 500 });
+  }
+}
