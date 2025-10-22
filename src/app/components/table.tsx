f203fbcfb7da30b7d04ef2c48f0d0fb585e93e28
Mahdi <mahdi.mousavi0011@gmail.com>
2025-10-22 12:12:32 +0200

    Add onclick function to table component.

--- src/app/components/table.tsx
+++ src/app/components/table.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 interface TableProps {
-  columns: string[]; // rubriker
-  data: Array<Record<string, string | number>>; // listan med objekt
+  columns: string[];
+  data: Array<Record<string, string | number>>;
+  tableName: string;
+  onRowClick?: (id: string | number, tableName: string) => void;
 }
 
-export default function Table({ columns, data }: TableProps) {
+export default function Table({ columns, data, tableName, onRowClick }: TableProps) {
   return (
     <div className="overflow-x-auto">
       <table className="min-w-full border border-gray-400 rounded-lg shadow-sm">
@@ -20,7 +22,11 @@ export default function Table({ columns, data }: TableProps) {
         </thead>
         <tbody>
           {data.map((row, i) => (
-            <tr key={i}>
+            <tr
+              key={i}
+              className="hover:bg-gray-100 cursor-pointer"
+              onClick={() => onRowClick && onRowClick(row.id, tableName)} // när man klickar visar detaljer för den raden
+            >
               {columns.map((col) => (
                 <td key={col} className="py-2 px-4 border-b">
                   {row[col]}
